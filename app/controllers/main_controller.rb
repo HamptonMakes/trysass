4f533cf0f70a7c999100f23df0de7488c48c2d7b
Michael JL Catlin <malrase@gmail.com>
2013-07-20 14:02:29 -0700

    Adding about page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,10 @@
 class MainController < ApplicationController
   def index
   end
-
+  
+  def about
+  end
+  
   def compile
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Trysass::Application.routes.draw do
-  get "main/index"
-  get "main/compile"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get '/:action' => 'main'
   # You can have the root of your site routed with "root"
   root 'main#index'
 
