ad74863b17360991193030e4f0fdeadb08bf06d2
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2013-07-20 14:03:34 -0700

    getting basic compilation going

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,12 @@ class MainController < ApplicationController
   end
 
   def compile
+  	engine = Sass::Engine.new(params[:template],
+  							  :load_paths => [],
+  							  :filesystem_importer => Sass::Importers::Base)
+ 	css = engine.render
+ 	render :json => {
+ 		:result => engine.css
+ 	}
   end
 end
