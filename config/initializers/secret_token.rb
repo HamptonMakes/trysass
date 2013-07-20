3dbc22b9b8c5c1695efad14189628abebeb32313
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2013-07-20 13:21:25 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trysass::Application.config.secret_key_base = '9efc3230909d5d77262edfd1dd304c61fa50f1a2e6a1b5a37674afa2741d029b9f662dfcdd2bdf62d442baa0304430d3b0e43c2cc0b4f1cf4f7b11a66fa97122'
